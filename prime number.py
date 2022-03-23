5844eca59478e59b9527b925cfef64de5335eb98
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-03-23 18:14:51 +0530

    Create prime number.py
    geeksforgeeks basic problem

---
+++ prime number.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+import math
+class Solution:
+    def isPrime (self, N):
+        if (N==1):
+           return 0
+        for i in range(2,math.floor(math.sqrt(N)+1)):
+           if N%i == 0:
+               return 0
+        return 1
