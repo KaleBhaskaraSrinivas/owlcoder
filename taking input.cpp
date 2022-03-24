b21b7b1243d9e8eb8abf4385ed9a21a566d5d7a7
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-03-24 17:24:21 +0530

    Create taking input.cpp

---
+++ taking input.cpp
@@ -0,0 +1,8 @@
+ void inputData()
+{
+       int a;
+       string b;
+       cin>>a>>b;
+       cout<<a<<" "<<b;
+      //Use cin to take input and cout a and b with a space between them. Also use an endl after output
+}
