2baaf7eae0d88b7eddf35edb8856e7cf771a366b
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-03-29 22:27:39 +0530

    Update Max Sum Subarray of size K .py

--- Max Sum Subarray of size K .py
+++ Max Sum Subarray of size K .py
@@ -1,8 +1,7 @@
 class Solution:
     def maximumSumSubarray (self,K,Arr,N):
        s=0
-       t=K
        for i in range(0,N):
-           s=max(s,sum(Arr[i:t]))
-           t+=1
+           s=max(s,sum(Arr[i:K]))
+           K+=1
        return s
