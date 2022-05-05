0a41e2f71f445f24a5c4d140cc12341e3b5bc673
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2022-05-05 17:52:50 +0530

    Create Check for BST.cpp

---
+++ Check for BST.cpp
@@ -0,0 +1,10 @@
+int prev=INT_MIN;
+    bool isBST(Node* root) 
+    {
+         
+         if(root==NULL) return true;
+         if(isBST(root->left)==false) return false;
+         if(root->data <=prev) return false;
+         prev=root->data;
+         return isBST(root->right);
+    }
